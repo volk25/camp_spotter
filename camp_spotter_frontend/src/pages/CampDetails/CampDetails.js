37bc15e3183821d9d1653aa37956927e28609b7b
volk25 <nikita.volkov25@gmail.com>
2022-02-09 22:38:40 +0100

    Cors headers management added to the backend

--- camp_spotter_frontend/src/pages/CampDetails/CampDetails.js
+++ camp_spotter_frontend/src/pages/CampDetails/CampDetails.js
@@ -5,24 +5,24 @@ import { MapContainer, TileLayer, Marker} from 'react-leaflet';
 import { GetIcon } from '../../components/Map';
 import Navbar from "../../components/Navbar/Navbar";
 
-
+/**
+ * Render the camp detail page with camp title, image, main body, position body, map position, and reviews. 
+ * @returns renders the page
+ */
 function CampDetails () {
 
     // Define the variables/constants/states
     const [campDetails, setCampDetails] = useState([]);
     const [reviewList, setReviewList] = useState([]); 
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [campError, setCampError] = useState();
     const [reviewError, setReviewError] = useState();
-
-
-    
 
     // Define what to do when the page is loaded
     useEffect (() => {
 
         // Fetch the data from the API (keep in mind that the current requesting address should be authorized in the API)
-        fetch ('http://127.0.0.1:8000/camps/camp-rome/', {
+        fetch ('http://127.0.0.1:8000/camps/camp-california/', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -42,7 +42,7 @@ function CampDetails () {
         // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         // Fetch the data from the API (keep in mind that the current requesting address should be authorized in the API)
-        fetch ('http://127.0.0.1:8000/camps/camp-rome/reviews/', {
+        fetch ('http://127.0.0.1:8000/camps/camp-california/reviews/', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -61,69 +61,84 @@ function CampDetails () {
 
     },[]);
 
-
-    //in case data will be fetched slowly, "data loading" will appear on the screen.
+    // If loading variable is still set to true, notify it to the user
     if (loading) {
         return <p>Data is loading...</p>;
       }
     //if json doesn't have an array, this error will be displayed on the screen. For map function to work, json has to be an array
     if (campError) {
-    return <p>An error occurred while loading the Camp details!</p>;
+        return <p>An error occurred while loading the Camp details!</p>;
     }
     if (reviewError) {
         return <p>An error occurred while loading the Review List!</p>;
-        }
+    }
+
+    // Define the reviews snippet to be inserted in the main return
+    let reviews = <div className='text-white fw-italic fs-5'>This camp site has no reviews yet...</div>
+    if (reviewList.length>0) {
+        reviews = reviewList.map(review => (                      
+            <div key={review.id}>                       
+                <div className='d-flex justify-content-evenly w-20'  >
+                    <div className='me-3'>
+                        <img src={review.image} alt="user image" width="70" height= "70"></img>
+                        <div className='w-25 text-center text-black'>
+                            {review.author}
+                        </div>
+                    </div>
+                    <div className= 'p-3 mb-2 bg-secondary bg-opacity-50 text-white rounded-pill w-75'>
+                        {review.body}
+                    </div>   
+                </div>    
+            </div>  
+        ))
+    }
 
     // If data is loaded and there are no errors, show the CampDetails page
     return (
         
         <div> 
-        
+
             <div className='overlay'></div>
             <div className='container position-relative'>
-                <div className='mb-3'><Navbar/></div>
+
+                {/* Navbar on the page */}
+                <div className='mb-3'>
+                    <Navbar/>
+                </div>
             
-                    {/* Camp image with camp name displayed on it */}
-                    <div className="position-relative text-center" >
-                        <img src={campDetails.image} className="radius" width="100%" height= "200" alt="mainCampImage" />
-                        <div className='box bg-secondary bg-opacity-75'>
-                            <p className="position-absolute  bottom-0 start-50 translate-middle-x  text-white fs-2">{campDetails.title}</p>
-                        </div>
+                {/* Camp image with camp name displayed on it */}
+                <div className="position-relative text-center" >
+                    <img src={campDetails.image} className="radius" width="100%" height= "200" alt="mainCampImage" />
+                    <div className='box bg-secondary bg-opacity-75'>
+                        <p className="position-absolute  bottom-0 start-50 translate-middle-x  text-white fs-2">
+                        {campDetails.title}
+                        </p>
                     </div>
-                    
-                    {/* Main description of the camp */}
-                    <div className='mt-3 mb-3 p-3 text-white h5 bg-secondary bg-opacity-50 radius'>{campDetails.main_body}</div>  
-
-                    {/* Position description on the left and map on the right */}
-                    <div className='d-flex mb-3'>
-                        <div className= 'radius p-3 mb-2 bg-secondary bg-opacity-50 text-white h6 w-50'>{campDetails.position_body}</div>  
-                        <div className='w-50 ms-2 radius'>
-                            {console.log(campDetails)}
-                            {/* <MapContainer className="map" center={[campDetails.latitude, campDetails.longitude]} zoom={20} style={{height:"30vh"}}> 
-                                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 
-                                    contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>  
-                                    <Marker position={[campDetails.latitude, campDetails.longitude]} icon={GetIcon(50)}> </Marker>
-                            </MapContainer>       */}
-                        </div>                           
-                    </div> 
-
-                    {/* Reviews part */}
-                    <div className='text-white fw-bold fs-2'>Reviews</div> 
-
-                    {reviewList.map(review => (                      
-                    <div key={review.id}>                       
-                        <div className='d-flex justify-content-evenly w-20'  >
-                            <div className='me-3'>
-                                <img src={review.image}  alt="user image" width="70" height= "70"></img>
-                                <div className='w-25 text-center text-black'>{review.author}</div>
-                            </div>
-                            
-                            <div className= 'p-3 mb-2 bg-secondary bg-opacity-50 text-white rounded-pill w-75'>{review.body}</div>   
-                        </div>    
+                </div>
+                
+                {/* Details of the camp */}
+                <div className='mt-3 mb-3 p-3 text-white h5 bg-secondary bg-opacity-50 radius'>
+                    {campDetails.main_body}
+                </div>
+                <div className='d-flex mb-3'>
+                    <div className= 'radius p-3 mb-2 bg-secondary bg-opacity-50 text-white h6 w-50'>
+                        {campDetails.position_body}
                     </div>  
-                ))} ,
-                    
-            
+                    <div className='w-50 ms-2 radius'>
+                        <MapContainer className="map" center={[campDetails.latitude, campDetails.longitude]} zoom={20} style={{height:"30vh"}}> 
+                            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 
+                                contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>  
+                            <Marker position={[campDetails.latitude, campDetails.longitude]} icon={GetIcon(50)}/>
+                        </MapContainer>      
+                    </div>                           
+                </div> 
+
+                {/* Reviews of the camp, if present */}
+                <div className='text-white fw-bold fs-2'>Reviews</div>
+                <div>
+                    {reviews}
+                </div>
+                            
             </div>  
         </div>
     )}
--- camp_spotter_frontend/src/pages/MapPage/MapPage.js
+++ camp_spotter_frontend/src/pages/MapPage/MapPage.js
@@ -1,16 +1,17 @@
-
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import MyMap from "../../components/Map"
 
-
+/**
+ * Render the map page with all the camps. 
+ * @returns renders the page
+ */
 const MapPage = () => {
   return (
     <div className="bg-success">
-      <Navbar />
-      <MyMap />
+      <Navbar/>
+      <MyMap/>
     </div>
-
   );
 }
 
