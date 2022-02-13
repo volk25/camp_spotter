7f58d93194cebeeb4e273cdd422801547f13f7cf
maryna <maryna.finko@gmail.com>
2022-02-13 12:08:31 +0100

    small update in camp details

--- camp_spotter_frontend/src/pages/CampDetails/CampDetails.js
+++ camp_spotter_frontend/src/pages/CampDetails/CampDetails.js
@@ -82,7 +82,7 @@ function CampDetails () {
     if (reviewList.length > 0) {
         reviews = reviewList.map(review => (                      
             <div key={review.id}>                       
-                <div className='d-flex justify-content-evenly w-20'  >
+                <div className='d-flex justify-content-evenly'  >
                     <div className='me-3'>
                         <img src={review.author_image} alt="user image" width="70" height= "70"/>
                         <div className='w-25 text-center text-black'>
@@ -103,13 +103,11 @@ function CampDetails () {
         <div> 
 
             <div className='overlay'></div>
-            <div className='container position-relative'>
-
-                {/* Navbar on the page */}
-                <div className='mb-3'>
-                    <Navbar/>
-                </div>
-            
+            {/* Navbar on the page */}
+            <div className='mb-5 position-relative'>
+                <Navbar/>
+            </div>
+            <div className='container position-relative'>           
                 {/* Camp image with camp name displayed on it */}
                 <div className="position-relative text-center" >
                     <img src={campDetails.image} className="radius" width="100%" height= "200" alt="mainCampImage" />
@@ -128,7 +126,7 @@ function CampDetails () {
                     <div className= 'radius p-3 mb-2 bg-secondary bg-opacity-50 text-white h6 w-50'>
                         {campDetails.position_body}
                     </div>  
-                    <div className='w-50 ms-2 radius'>
+                    <div className='w-50 ms-2 radius bg-secondary bg-opacity-50 h6'>
                         <MapContainer className="map" center={[campDetails.latitude, campDetails.longitude]} zoom={20} style={{height:"30vh"}}> 
                             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 
                                 contributors'url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>  
