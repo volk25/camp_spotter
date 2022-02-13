5c04ce46dec6c2fa49cccce6f5ed125b79f552a3
maryna <maryna.finko@gmail.com>
2022-02-13 09:55:03 +0100

    Added Login page

--- camp_spotter_frontend/src/App.js
+++ camp_spotter_frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/Login";
+// import Signup from "./pages/Signup/Signup";
 import Navbar from "./components/Navbar/Navbar";
 import MapPage from "./pages/MapPage/MapPage";
 import CampDetails from "./pages/CampDetails/CampDetails";
@@ -14,9 +16,11 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/map" element={<MapPage />} />
-          <Route  path="/camps/:slug" element={<CampDetails/>} />
+          <Route exact path="/login" element={<Login/>}/>
+          {/* <Route exact path="/signup" element={<Signup />} /> */}
+          <Route exact path="/contact" element={<Contact />}/>
+          <Route exact path="/map" element={<MapPage />}/>
+          <Route  path="/camps/:slug" element={<CampDetails/>}/>
 
         </Routes>
       </div>
---
+++ camp_spotter_frontend/src/pages/Login/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Navbar />
+      <h1 className="login-header">Login</h1>
+      <div style={{ height: "50vh" }}> </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="form-group" size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="form-group" size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
