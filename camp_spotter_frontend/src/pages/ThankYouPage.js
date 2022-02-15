d27f47650e08e9766d82c420f57b8c590d03b06d
maryna <maryna.finko@gmail.com>
2022-02-15 19:15:17 +0100

    thank you page

---
+++ camp_spotter_frontend/src/pages/ThankYouPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default  function ThankYou() {
+    return(
+        <div className="text-white text-center h1 mt-5">
+            <div>Thank you!</div>
+            <div style={{ height: "100vh" }}></div>
+        </div>
+    )
+}
