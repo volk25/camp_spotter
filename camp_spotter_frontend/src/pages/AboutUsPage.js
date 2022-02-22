1c11e0506cf0ce5a6327abd9ccb6303e5407658b
maryna <maryna.finko@gmail.com>
2022-02-22 17:04:27 +0100

    minor changes

--- camp_spotter_frontend/src/pages/AboutUsPage.js
+++ camp_spotter_frontend/src/pages/AboutUsPage.js
@@ -16,20 +16,13 @@ export default function AboutUsPage() {
 
       {/* About Us Text */}
       <div className="background text-center text-white mt-5">
+      
         <h1 className="contact-header">About Us</h1>
       </div>
 
       {/* About us text */}
-      <div className="text-white mt-5 h5 w-50 aboutUs">
-        Magna sint do non ipsum duis velit quis ea magna enim tempor aute
-        sit deserunt. Officia veniam sint voluptate eiusmod nostrud sunt
-        deserunt labore cillum elit est deserunt ut. Occaecat aute duis
-        aliqua ad. Ullamco in enim enim laboris esse incididunt amet. Ad
-        deserunt ut cupidatat id veniam velit elit labore occaecat
-        occaecat incididunt consequat ipsum. Voluptate aliqua ea cupidatat
-        reprehenderit dolore enim occaecat officia commodo voluptate ut
-        veniam. Nostrud ea aliqua proident eu ea irure do sit dolor
-        proident esse culpa.
+      <div className="text-white  text-center mt-5 h5 w-50 aboutUs">
+      While being a true nature lovers and loving camping, we realized that there are not enough reliable information sources about where to stay overnight in a truly wild and beautiful place, but also a safe one.  And we mean places, where you will not be surrounded with lots of other tents and campers. We wanted to create a community of camping lovers, who will not only share their favorite wild camping spots, but also will be able to interact and comment/review those spots, where they have previously stayed.  And all this in order to inspire other people to spend some time close to the nature and wake up with a stunning view. That’s why we decided to build an application which sets the basis for a great start.
       </div>
 
       {/* Insert the contact form */}
